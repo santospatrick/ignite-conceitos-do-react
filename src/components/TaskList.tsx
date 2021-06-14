073d4aee89cf02c006ebd688d52f53bbbe692e60
Patrick Santos <patrickspteixeira@gmail.com>
2021-06-14 17:29:16 -0300

    adiciona funcoes de crud

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,24 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
+    if (!newTaskTitle) return
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    setTasks(prevState => [
+      ...prevState, 
+      {
+        id: Math.random() * 1000,
+        title: newTaskTitle, 
+        isComplete: false
+      }
+    ])
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(prevState => prevState.map(item => item.id === id ? { ...item, isComplete: !item.isComplete } : item))
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    setTasks(prevState => prevState.filter(item => item.id !== id))
   }
 
   return (
